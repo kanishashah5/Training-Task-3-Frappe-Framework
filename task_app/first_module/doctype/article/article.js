ad1f667be4ea4f50e7e72a39dec6f29fcb2a8d46
Kanisha Shah <kanishah0505@gmail.com>
2025-02-11 12:21:07 +0530

    Added Web View Page

---
+++ task_app/first_module/doctype/article/article.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Sanskar Technolab and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Article", {
+// 	refresh(frm) {
+
+// 	},
+// });
