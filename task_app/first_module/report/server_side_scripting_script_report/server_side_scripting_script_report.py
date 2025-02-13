1757f7ff8be6c13374af68a8108961b2644fe311
Kanisha Shah <kanishah0505@gmail.com>
2025-02-13 18:48:17 +0530

    Dashboard

--- task_app/first_module/report/server_side_scripting_script_report/server_side_scripting_script_report.py
+++ task_app/first_module/report/server_side_scripting_script_report/server_side_scripting_script_report.py
@@ -28,8 +28,9 @@ def execute(filters=None):
 			})
 		data.append(row)
 
-	return columns, data
-
+	chart = get_chart_data(data)
+	report_summary = get_report_summary(data)
+	return columns, data, None, chart,report_summary
 
 def get_columns():
 	return [
@@ -71,3 +72,65 @@ def get_conditions(filters):
 			conditions[key] = value
 
 	return conditions
+
+def get_chart_data(data):
+	if not data:
+		return None
+
+	labels = ['Age <= 45','Age > 45']
+
+	age_data = {
+		'Age > 45': 0,
+		'Age <= 45': 0,
+	}
+	datasets = []
+
+	for entry in data:
+		if entry.age <= 45:
+			age_data['Age <= 45'] += 1
+			
+		else:
+			age_data['Age > 45'] += 1
+
+	datasets.append({
+		'name': 'Age Status',
+		'values': [age_data.get('Age <= 45'),age_data.get('Age > 45')]
+	})
+
+	chart = {
+		'data': {
+			'labels': labels,
+			'datasets': datasets
+		},
+		'type': 'pie',
+		'height': 300,
+	}
+
+	return chart
+
+def get_report_summary(data):
+	if not data:
+		return None
+
+	age_below_45, age_above_45 = 0, 0
+
+	for entry in data:
+		if entry.age <= 45:
+			age_below_45 += 1
+			
+		else:
+			age_above_45 += 1
+	return [
+		{
+			'value': age_below_45,
+			'indicator': 'Green',
+			'label': 'Age Below 45',
+			'datatype': 'Int',
+		},
+		{
+			'value': age_above_45,
+			'indicator': 'Red',
+			'label': 'Age Above 45',
+			'datatype': 'Int',
+		}
+	]
