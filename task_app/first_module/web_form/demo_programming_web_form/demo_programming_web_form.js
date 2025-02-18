7a0c5fc38dd93405a331a6ceada35e2dcf45acfd
Kanisha Shah <kanishah0505@gmail.com>
2025-02-18 19:08:25 +0530

    Jinja API

--- task_app/first_module/web_form/demo_programming_web_form/demo_programming_web_form.js
+++ task_app/first_module/web_form/demo_programming_web_form/demo_programming_web_form.js
@@ -1,7 +1,13 @@
 frappe.ready(function() {
-	// bind events here
+    setTimeout(() => {
+        if (typeof frappe.web_form !== "undefined") {
+            console.log("Web Form Loaded:", frappe.web_form);
 
-	frappe.web_form.after_load = () => {
-		frappe.msgprint('Please fill all values carefully');
-	}
+            frappe.web_form.on('render', function() {
+                frappe.msgprint('Please fill all values carefully');
+            });
+        } else {
+            console.error("frappe.web_form is not available.");
+        }
+    }, 1000); // 1-second delay
 });
