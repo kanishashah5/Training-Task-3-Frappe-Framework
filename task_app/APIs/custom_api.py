0e628a3368195a0ee75809c5e86dc1864bf5dccc
Kanisha Shah <kanishah0505@gmail.com>
2025-02-25 19:04:48 +0530

    Custom API

--- task_app/APIs/custom_api.py
+++ task_app/APIs/custom_api.py
@@ -1,6 +1,13 @@
 import frappe
 
 
+# -------------------Can be accessed publicly---------------- 
+
+# @frappe.whitelist(allow_guest=True)
+# def greet():
+#     return {"message": "Hello from Custom API!"} 
+
+# -----------------------Only authorised users can access this-------------
 @frappe.whitelist()
 def greet():
     return {"message": "Hello from Custom API!"} 
